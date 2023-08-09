aeb24bee36ee343bda060af4420fcb4f4f55c68c
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2023-08-09 14:45:54 +0100

    Add functionality to extract ROIs for each file
    Multiple ROIs can be extracted for analysis.

--- focus_finder.py
+++ focus_finder.py
@@ -8,6 +8,7 @@ from focus_finder_gui import pointSelectGUI
 from matplotlib.gridspec import GridSpec
 from astropy.modeling import models, fitting
 
+
 def extract_variables_and_export(filenames, pattern, column_names=None):
     # Initialize lists to store extracted variables
     variable_data = []
@@ -35,6 +36,67 @@ def extract_variables_and_export(filenames, pattern, column_names=None):
     return df
 
 
+# Write a function which takes a 2D array, an X and Y coordinate, and a box size
+# and returns a 2D array of the box centered on the X and Y coordinate. The
+# function should also check that the box is within the bounds of the array.
+def get_box(array, x, y, box_size=30):
+    # Get the shape of the array
+    ny, nx = array.shape
+
+    # Check that the box is within the bounds of the array
+    if x - box_size < 0 or x + box_size > nx or y - box_size < 0 or y + box_size > ny:
+        raise ValueError("Box is not within the bounds of the array")
+
+    x_discrete = int(x)
+    y_discrete = int(y)
+    
+    # Get the box
+    box = array[y_discrete - box_size:y_discrete + box_size, x_discrete - box_size:x_discrete + box_size]
+
+    return box
+
+
+# write a function which takes reads a fits file and returns the data as a 2D array
+def read_fits(filename, ext=0):
+    # Read the fits file
+    with fits.open(filename) as hdul:
+        data = hdul[ext].data
+        header = hdul[ext].header
+    
+    return data, header
+
+
+# write a function which takes a list of fits files, and a list of X, Y
+# coordinates as input. The function should read the fits files using read_fits
+# and extract the box around the X, Y coordinates using get_box for each file. It
+# should then return a 3D array of the boxes.
+def get_boxes_from_files(fits_files, X, Y, box_size=30):
+    """
+    Process a list of FITS files and extract boxes around given coordinates.
+    
+    :param fits_files: List of FITS file paths.
+    :param coordinates_list: List of (x, y) coordinates.
+    :param box_size: Size of the box around each coordinate.
+    :return: A dictionary with filenames as keys and 3D arrays of boxes as values.
+    """
+    result = {}
+    boxes = np.zeros((len(X), box_size*2, box_size*2))
+    # Loop through each filename and coordinates
+    for filename in fits_files:
+        n = 0
+        for x, y in zip(X, Y):
+            # Read the fits file
+            data, header = read_fits(filename)
+
+            # Get the box
+            box = get_box(data, x, y, box_size=box_size)
+            boxes[n, :, :] = box
+            n += 1
+        result[filename] = boxes
+        
+    return result
+
+
 def main():
     # Sample list of filenames
     filenames = glob.glob("data/raw/test_3A.01.05/test*.fits")
@@ -57,6 +119,19 @@ def main():
     gui = pointSelectGUI(fn_list)
     gui.run()
     box_centres = gui.selection['Selected Points']
+    
+    box_arr = get_boxes_from_files(fn_list, box_centres[:,0], box_centres[:,1])
+
+    # Plot the boxes for a given file
+    nplots = len(box_centres)
+    ncols = 2 
+    nrows = np.ceil(nplots/ncols).astype(int)
+    
+    fig, ax = plt.subplots(nrows=1, ncols=nplots)
+    for i in range(nplots):
+        ax[i].imshow(box_arr[fn_list[0]][i], origin='lower', vmin=0, vmax=1000)
+        
+    plt.show() 
     
     
     
