5a7179f2bf86d21cbb4faea69520566f239a43e7
Ciaran Breen <ciaran.breen@stfc.ac.uk>
2023-08-25 12:21:18 +0100

    Show outliers in spline fit

--- focus_plane_fitting.py
+++ focus_plane_fitting.py
@@ -81,15 +81,16 @@ def find_point_on_plane(A, B, C, D, known_coords, missing_coord='z'):
 # write a function which takes Z and a score as input and fits a spline to the
 # score. Then find the Z value at the minimum of the spline over the range of Z
 # if there are any outliers, remove them and refit the spline
-def fit_spline(Z, score, k=4, outlier_f=1.5, minZ_step=0.01):
+def fit_spline(Z, score, k=5, outlier_f=1000000, minZ_step=0.01):
     
     # fit the spline
     spline_1D = UnivariateSpline(Z, score, k=k)
     
     # outlier removal using residual
     residual = score - spline_1D(Z)
+    print(residual)
     # find the outliers
-    outliers_mask = np.abs(residual) > outlier_f * np.std(residual)
+    outliers_mask = np.abs(residual) > (outlier_f * np.std(residual)) + np.mean(residual)
     # remove the outliers present and refit the spline
     # TODO plot the outliers separately with different marker
     if outliers_mask.sum() > 0:
@@ -102,7 +103,7 @@ def fit_spline(Z, score, k=4, outlier_f=1.5, minZ_step=0.01):
     spline_min_Z = spline_points[np.argmin(spline_1D(spline_points))]
     spline_min_score = spline_1D(spline_min_Z)
     
-    return spline_1D, (spline_min_Z, spline_min_score)
+    return spline_1D, (spline_min_Z, spline_min_score), outliers_mask
 
 
 def create_subplot_grid(num_plots):
@@ -122,7 +123,7 @@ def create_subplot_grid(num_plots):
 
 # write a function which finds the minimum of a score as a function of Z for
 # each box id
-def find_minima(coords, score, ID_arr, DAM_stoutlier_f=1.5, minZ_step=0.01, 
+def find_minima(coords, score, ID_arr, DAM_stoutlier_f=5, minZ_step=0.01, 
                 score_label='Score', limit=None):
     # make sure the arrays are numpy arrays
     coords = np.array(coords)
@@ -159,7 +160,7 @@ def find_minima(coords, score, ID_arr, DAM_stoutlier_f=1.5, minZ_step=0.01,
         # Z is found using spline fit
         Z_id = coords_id[:,2]
         score_id = score[ID_arr == ID]
-        spline_fit, spline_min = fit_spline(Z_id, score_id, outlier_f=DAM_stoutlier_f, minZ_step=minZ_step)
+        spline_fit, spline_min, mask = fit_spline(Z_id, score_id, outlier_f=DAM_stoutlier_f, minZ_step=minZ_step)
         
         # store the result
         minima[n] = np.array([X_min, Y_min, spline_min[0]])
@@ -177,9 +178,12 @@ def find_minima(coords, score, ID_arr, DAM_stoutlier_f=1.5, minZ_step=0.01,
             
         
         # sanity check the spline minima with a plot
-        axes[n].scatter(Z_id, score_id)
-        axes[n].plot(spline_points, spline_fit(spline_points))
-        axes[n].scatter(spline_min[0], spline_min[1], color='r')
+        axes[n].scatter(Z_id[~mask], score_id[~mask], marker='o', color='blue', 
+                        label='Included Points')
+        axes[n].scatter(Z_id[mask], score_id[mask], marker='o', color='b', 
+                        facecolors='none', label='Excluded Points')
+        axes[n].plot(spline_points, spline_fit(spline_points), color='r', label='Spline Fit')
+        axes[n].scatter(spline_min[0], spline_min[1], color='r', label='Spline Min')
         axes[n].set_xlabel('Z')
         axes[n].set_ylabel(score_label)
         axes[n].set_title(f'Point ID = {ID}')
@@ -279,7 +283,7 @@ def main():
     for n, metric in enumerate(args.metrics):
         # find the minimum of the metric as a function of Z
         IDs, minima = find_minima(coords, metrics_df[metric], metrics_df['Point ID'], 
-                                  DAM_stoutlier_f=1.5, minZ_step=0.01, score_label=metric)
+                                  DAM_stoutlier_f=5, minZ_step=0.01, score_label=metric)
         
         # fit a plane to the spline minima
         (A, B, C, D) = plane_fitter(minima)
@@ -294,7 +298,7 @@ def main():
     # find the minimum of the weighted sum of metrics as a function of Z for
     # each DAM
     score_IDs, score_minima = find_minima(coords, mixed_score, metrics_df['Point ID'], 
-                                           DAM_stoutlier_f=1.5, minZ_step=0.01,
+                                           DAM_stoutlier_f=5, minZ_step=0.01,
                                            score_label='Mixed Score')
 
     (A, B, C, D) = plane_fitter(score_minima)
